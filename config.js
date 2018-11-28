cbe8d716736d7763e49e3f84ea241d82633ee234
jmejiavazquez2008 <44645252+jmejiavazquez2008@users.noreply.github.com>
2018-11-28 16:56:58 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-exports.PORT = process.env.PORT || 3001; // use heroku's dynamic port or 3001 if localhost
+exports.PORT = process.env.PORT || 3000; // use heroku's dynamic port or 3001 if localhost
 exports.DEBUG = true;
 exports.ENVIRONMENT = 'sandbox';
-exports.CALLBACK_URL = 'http://localhost:3001';
+exports.CALLBACK_URL = 'http://localhost:3000';
 
 exports.CLIENT_ID = process.env.CLIENT_ID || "3MVG9Vik22TUgUpheZzEWUZ9QDnGeMnM8OGZBaUuUXdFQvKfvju4EoT9hS1jgUQpO9pbtIxk37dIIn0thfEMF";
 exports.CLIENT_SECRET = process.env.CLIENT_SECRET || "7984638764575275172";
