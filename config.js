444aa6d3617e20278c6fcb1bf5644dc0391d084f
jmejiavazquez2008 <44645252+jmejiavazquez2008@users.noreply.github.com>
2018-11-28 16:32:09 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+exports.PORT = process.env.PORT || 3001; // use heroku's dynamic port or 3001 if localhost
+exports.DEBUG = true;
+exports.ENVIRONMENT = 'sandbox';
+exports.CALLBACK_URL = 'http://localhost:3001';
+
+exports.CLIENT_ID = process.env.CLIENT_ID || "3MVG9Vik22TUgUpheZzEWUZ9QDnGeMnM8OGZBaUuUXdFQvKfvju4EoT9hS1jgUQpO9pbtIxk37dIIn0thfEMF";
+exports.CLIENT_SECRET = process.env.CLIENT_SECRET || "7984638764575275172";
+exports.USERNAME = process.env.USERNAME || "jmejiavazquez@huronconsultinggroup.com";
+exports.PASSWORD = process.env.PASSWORD || "Interscope_2008";
