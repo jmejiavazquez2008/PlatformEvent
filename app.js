a7c1be56a098a07750c995c45f3264b2f807d181
jmejiavazquez2008 <44645252+jmejiavazquez2008@users.noreply.github.com>
2018-12-03 11:35:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var config = require('./config.js');
+//var config = require('./config.js');
 var nforce = require('nforce');
 var faye = require('faye');
 
