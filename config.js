4f27299395de19574d4301fbd0fbad669040fa8a
jmejiavazquez2008 <44645252+jmejiavazquez2008@users.noreply.github.com>
2018-12-10 13:24:56 -0500

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// exports.PORT = process.env.PORT || 3000; // use heroku's dynamic port or 3001 if localhost
-// exports.DEBUG = true;
-// exports.ENVIRONMENT = 'sandbox';
-// exports.CALLBACK_URL = 'http://localhost:3000';
-
-// exports.CLIENT_ID = process.env.CLIENT_ID || "3MVG9Vik22TUgUpheZzEWUZ9QDnGeMnM8OGZBaUuUXdFQvKfvju4EoT9hS1jgUQpO9pbtIxk37dIIn0thfEMF";
-// exports.CLIENT_SECRET = process.env.CLIENT_SECRET || "7984638764575275172";
-// exports.USERNAME = process.env.USERNAME || "jmejiavazquez@huronconsultinggroup.com";
-// exports.PASSWORD = process.env.PASSWORD || "Interscope_2008";
